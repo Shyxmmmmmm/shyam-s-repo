9635ebb8fefad34dd7ea9f8875e89db78ed91429
Shyxmmmmmm <shyamkumar2005464@gmail.com>
2025-08-10 20:54:38 +0530

    p4.py

--- p4.py
+++ p4.py
@@ -14,4 +14,24 @@ class Solution:
 
 sol=Solution()
 m=int(input("Input:"))
+
+sol.p(m)
+
+
+#time complixity matters 
+
+class Solution:
+    def p(self,N):
+        for i in range(1,N+1):
+            for j in range(1,i+1):
+                print("*",end="")
+            print()
+            
+        for i in range(1,N):
+            for j in range(1,N+1-i):
+                print("*",end='')
+            print()
+
+sol=Solution()
+m=int(input("Input:"))
 sol.p(m)
