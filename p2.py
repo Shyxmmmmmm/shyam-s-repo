49f63349aaa484a785a8919af3e6b7cb045d2f79
Shyxmmmmmm <shyamkumar2005464@gmail.com>
2025-08-10 20:23:52 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def p(self,N):
+        for i in range(1,N+1):
+            val=N  #remember val is a temp variable to store the N value.coz if you directly run the N value means it'll works for first [i=1] only "coz the N value is already 0"(so store the N value in temp before assing the row loop)
+            for j in range(1,val-i+2):
+                print(val,end=" ")
+                val=val-1
+            print()
+
+sol=Solution()
+N=int(input("Input:"))
+sol.p(N)
