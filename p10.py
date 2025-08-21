8aa51d82e4f44aca66cec50457c7234a754a7d3c
Shyxmmmmmm <shyamkumar2005464@gmail.com>
2025-08-22 00:34:11 +0530

    Add files via upload

---
+++ p10.py
@@ -0,0 +1,10 @@
+class solution:
+    def p(self,n):
+        count=0
+        while(n>0):
+            n=n//10
+            count=count+1
+        print(count)
+sol=solution()
+n=int(input())
+sol.p(n)
