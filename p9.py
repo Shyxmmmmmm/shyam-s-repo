47b6aa47f44e06aafeb7572894cad689129bb9e8
Shyxmmmmmm <shyamkumar2005464@gmail.com>
2025-08-22 00:25:43 +0530

    Add files via upload

---
+++ p9.py
@@ -0,0 +1,32 @@
+class Solution:
+    def p(self,n):
+        p=[]
+        while(n>0):
+            l=n%10
+            p.append(l)
+            n=n//10
+        print(p)
+
+
+sol=Solution()
+n=int(input("Enter the input:"))
+sol.p(n)
+        
+
+
+
+
+class Solution:
+    def p(self):
+        n=123456
+        c=[]
+        while (n>0):
+            l=n%10
+            c.append(l)
+            n=n//10
+        for i in reversed(c):
+            print(i,end=' ')
+sol=Solution()
+
+sol.p()
+          
