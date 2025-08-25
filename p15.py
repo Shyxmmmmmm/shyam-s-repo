d3ab10f15c20413f9a5394c921168d0d8ff37553
Shyxmmmmmm <shyamkumar2005464@gmail.com>
2025-08-25 22:12:35 +0530

    Create p15.py

---
+++ p15.py
@@ -0,0 +1,13 @@
+ip-5
+op-120 [palindrome]
+
+
+#User function Template for python3
+
+
+class Solution:
+    def factorial (self, n):
+        p=1
+        for i in range(1,n+1):
+            p=p*i
+        return p
