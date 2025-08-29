839a89ec098829ee3c457d61b904489428fb45a6
Shyxmmmmmm <shyamkumar2005464@gmail.com>
2025-08-29 21:10:52 +0530

    Update p17.py

--- p17.py
+++ p17.py
@@ -14,6 +14,24 @@ class Solution:
 
 
 
+class Solution:
+    def isPrime(self, n):
+        if n<=1:
+            return False
+        flag=0
+        for i in range(2,n):
+            if n%i==0:
+                flag=1
+                break
+        
+        if flag==1:
+            return False 
+        else:
+            return True
+
+
+
+
 
 
 def p():
