c691fd1d4e0a24a7dae246751c61f72d31851eb3
Shyxmmmmmm <shyamkumar2005464@gmail.com>
2025-08-30 21:46:59 +0530

    Update p26(important Reversing concept).py

--- p26(important Reversing concept).py
+++ p26(important Reversing concept).py
@@ -1,1 +1,23 @@
 Reversing concept important advise:
+
+def p():
+    n=12345
+    while n>0:
+        p=n%10
+        print(p,end=' ')
+        n=n//10
+p()
+## ithuvum reversing thaan ana intha method ah vachi namma oru oru int ah thaaan print panna mudiyum like if u want to print n=54321 it is not possible in this method
+
+
+
+def p():
+    n=12345
+    s=0
+    while n>0:
+        p=n%10
+        s=10*s+p
+        n=n//10
+    print(s)
+p()
+## learn this method ok because it is efficient that above one 
