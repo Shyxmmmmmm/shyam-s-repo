d43feadb6f45d86a4aa257c3dfafc6e702b3dcbe
Shyxmmmmmm <shyamkumar2005464@gmail.com>
2025-08-30 21:50:43 +0530

    Update p27(palindrome).py

--- p27(palindrome).py
+++ p27(palindrome).py
@@ -1,6 +1,9 @@
+Input: n = 555
+Output: true
+
 class Solution:
     def isPalindrome(self, n):
-        h=n
+        h=n  ### purinjikoo ipo n ah direct ah use panna while loop laa n ah vachi operation panni mudikum pothu atlast n=0 nu vanthuru so initial ah n ah vera or var la store panni vachikerathu safe 
         s=0
         while n>0:
             p=n%10
