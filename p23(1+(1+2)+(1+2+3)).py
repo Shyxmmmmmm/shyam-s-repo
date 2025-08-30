c01db0d41e56ec786d4f6a2fde9502a72d29a2ad
Shyxmmmmmm <shyamkumar2005464@gmail.com>
2025-08-30 18:38:57 +0530

    Update p23(1+(1+2)+(1+2+3)).py

--- p23(1+(1+2)+(1+2+3)).py
+++ p23(1+(1+2)+(1+2+3)).py
@@ -10,3 +10,23 @@ class Solution:
             s=s+i
             c=s+c
         return c
+
+
+
+def p():
+    n=5
+    s=0
+    for i in range(1,n+1):
+        p=i*(i+1)//2
+        s=s+p
+    print(s)
+    
+        
+p()
+
+it is important one
+
+def p():
+    n=5
+    print(n*(n+1)*(n+2)//6)
+p()
