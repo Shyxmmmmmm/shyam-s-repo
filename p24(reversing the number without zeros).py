54da1ec2b7c797d721e1e1634722cda9afb677b5
Shyxmmmmmm <shyamkumar2005464@gmail.com>
2025-08-30 21:08:12 +0530

    Create p24(reversing the number without zeros).py

---
+++ p24(reversing the number without zeros).py
@@ -0,0 +1,19 @@
+Input: n = 122
+Output: 221
+Explanation: By reversing the digits of number, number will change into 221.
+
+
+
+class Solution:
+	def reverseDigits(self, n):
+	    q=[]
+	    while n>0:
+	        p=n%10
+	        n=n//10
+	        q.append(p)
+	        
+	    s=''
+	    for i in q:
+	        if i!=0:
+	            s=s+str(i)
+	    return s
