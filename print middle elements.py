3711b8802c8c2d73c2f3fe02a1a1cc817cc1c4f8
Shyxmmmmmm <shyamkumar2005464@gmail.com>
2025-08-30 19:00:38 +0530

    Update print middle elements.py

--- print middle elements.py
+++ print middle elements.py
@@ -2,8 +2,10 @@ Input: a = 978, b = 518, c = 300
 Output: 518
 Explanation: Since 518>300 and 518<978, so 518 is the middle element.
 
-
-#User function Template for python3
+class Solution:
+    def middle(self, a, b, c):
+        a,b,c=sorted([a,b,c])
+        return b
 
 class Solution:
     def middle(self, a, b, c):
@@ -12,6 +14,12 @@ class Solution:
         
         mid=(a+b+c)-(mn+mx)
         return mid
+        
+        
+
+#User function Template for python3
+
+
         
         
         
