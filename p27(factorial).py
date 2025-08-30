07f78bfe87cc909f0412353e2b3859b07fed5c36
Shyxmmmmmm <shyamkumar2005464@gmail.com>
2025-08-30 21:52:59 +0530

    Create p27(factorial).py

---
+++ p27(factorial).py
@@ -0,0 +1,13 @@
+Examples :
+
+Input: n = 5
+Output: 120
+Explanation: 1 x 2 x 3 x 4 x 5 = 120
+
+class Solution:
+    def factorial(self, n: int) -> int:
+        fact=1
+        for i in range(1,n+1):
+            fact=fact*i
+        return fact
+        
