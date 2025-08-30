4048f05632842633e3ef544d932a5649bd3435b9
Shyxmmmmmm <shyamkumar2005464@gmail.com>
2025-08-30 21:49:25 +0530

    Create p27(palindrome).py

---
+++ p27(palindrome).py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, n):
+        h=n
+        s=0
+        while n>0:
+            p=n%10
+            s=s*10+p
+            n=n//10
+        if s==h:
+            return True
+        else:
+            return False
