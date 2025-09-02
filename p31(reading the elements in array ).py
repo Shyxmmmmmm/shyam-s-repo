ca0643fdb3ac30913f08572c600d29254238f02f
Shyxmmmmmm <shyamkumar2005464@gmail.com>
2025-09-02 21:44:56 +0530

    Create p31(reading the elements in array ).py

---
+++ p31(reading the elements in array ).py
@@ -0,0 +1,13 @@
+Input: arr[] = [1, 2, 3, 4, 5]
+Output: 1 2 3 4 5
+Input: arr[] = [2, 3, 5, 5]
+Output: 2 3 5 5
+
+
+
+#User function Template for python3
+class Solution:
+    # Just print the space seperated array elements
+	def printArray(self, arr):
+	    for i in arr:
+	        print(i,end=' ')
