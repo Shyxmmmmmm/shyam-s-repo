20d242ec3f422b010fd19a98b891d60976d8a5c6
Shyxmmmmmm <shyamkumar2005464@gmail.com>
2025-09-04 20:10:53 +0530

    Create p34(minimum and maximum elements of array are 1 and 10000).py

---
+++ p34(minimum and maximum elements of array are 1 and 10000).py
@@ -0,0 +1,20 @@
+Input: arr[] = [3, 2, 1, 56, 10000, 167]
+Output: 1 10000
+Explanation: minimum and maximum elements of array are 1 and 10000.
+
+Input: arr[] = [1, 345, 234, 21, 56789]
+Output: 1 56789
+Explanation: minimum and maximum element of array are 1 and 56789.
+
+Input: arr[] = [56789]
+Output: 56789 56789
+Explanation: Since the array contains only one element so both min & max are same.
+
+
+class Solution:
+    def get_min_max(self, arr):
+        s=sorted(arr)
+        a,b=s[0],s[-1]
+        return a,b
+            
+    
