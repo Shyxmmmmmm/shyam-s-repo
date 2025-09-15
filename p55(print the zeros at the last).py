7655d60bef0190a70b71c78caa668b2b200b1455
Shyxmmmmmm <shyamkumar2005464@gmail.com>
2025-09-15 19:24:52 +0530

    Update p55(print the zeros at the last).py

--- p55(print the zeros at the last).py
+++ p55(print the zeros at the last).py
@@ -13,3 +13,16 @@ class Solution:
 	    for i in range(j,n):
 	        arr[i]=0
 	    return arr
+
+optimized:
+
+class Solution:
+	def pushZerosToEnd(self, arr):
+	    n=len(arr)
+	    j=0
+	    for i in range(0,n):
+	        if arr[i]!=0:
+	            arr[i],arr[j]=arr[j],arr[i]
+	            j=j+1
+	   return arr
+	            
