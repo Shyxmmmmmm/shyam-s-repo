efac091a471263379579d3c3e8cab66b40f60765
Shyxmmmmmm <shyamkumar2005464@gmail.com>
2025-09-19 10:20:06 +0530

    Update p58STRING(PALINDROME).py

--- p58STRING(PALINDROME).py
+++ p58STRING(PALINDROME).py
@@ -10,3 +10,23 @@ if a==j:
     print(True)
 else:
     print(False)
+
+
+
+
+def p():
+    a='naaan'
+    n=len(a)
+    j=n-1
+    i=0
+    while(i<j):
+        if a[i]!=a[j]:
+            return False
+        j=j-1
+        i+=1
+    return True
+print(p())
+
+    
+        
+        
