c266f3585272cb39ff4f590664d282444aaebb47
Shyxmmmmmm <shyamkumar2005464@gmail.com>
2025-11-25 14:13:49 +0530

    Update p74(sort the single element).py

--- p74(sort the single element).py
+++ p74(sort the single element).py
@@ -33,5 +33,32 @@ class Solution:
                 if arr[i]!=arr[i+1] and arr[i]!=arr[i-1]:
                     return arr[i]
                 
+
+
+
+class Solution:
+    def single(self, arr):
+        n=len(arr)
         
+        if n==1:
+            return arr[0] 
+        if arr[0]!=arr[0+1]:
+            return arr[0] 
+        
+        if arr[n-1]!=arr[n-2]:
+            return arr[-1] 
+        
+        low=1
+        high=n-2
+        
+        while low<=high:
+            mid=(low+high)//2
+            
+            if arr[mid]!=arr[mid-1] and arr[mid]!=arr[mid+1]:
+                return arr[mid]
+            elif arr[mid]==arr[mid-1]:
+                low=mid+1
+            else:
+                high=mid-1
+                
         
